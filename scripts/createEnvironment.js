8d46506c4d43f7109be656a80d43976cde8a4dbf
yonas-t-a <yonas.tessema@aait.edu.et>
2025-06-12 12:23:51 +0300

    make the w and h small

--- scripts/createEnvironment.js
+++ scripts/createEnvironment.js
@@ -3,8 +3,8 @@ import * as THREE from 'three';
 function createFloor() {
     const floorTexture = new THREE.TextureLoader().load('textures/floor.jpg'); // Add a floor texture
     const floorMaterial = new THREE.MeshStandardMaterial({ map: floorTexture });
-    const floor = new THREE.Mesh(new THREE.PlaneGeometry(20, 20), floorMaterial);
-    floor.rotation.x = -Math.PI / 2; // Rotate to make it horizontal
+    const floor = new THREE.Mesh(new THREE.PlaneGeometry(10, 9), floorMaterial);
+    floor.rotation.x = -Math.PI / 2; 
     floor.receiveShadow = true;
     return floor;
 }
